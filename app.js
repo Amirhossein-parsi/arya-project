8ab0acd85604af4d5da31bc9cd6543057eb677d2
Amirhossein-parsi <aprs946@gmail.com>
2024-08-28 15:14:20 +0330

    header resposive complete

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ let menu = document.querySelector('.header__menu--low__title');
 let hiddenMenu = document.querySelector('.header__menu--low__items');
 let main = document.querySelector('.magazine');
 let articles = document.querySelector('.article');
+let bar = document.querySelector('.header__menu--upp__mobile-bars');
+let cancel = document.querySelector('.header__hamburger--cancel');
+let hamburgerMenu = document.querySelector('.header__hamburger');
 
 // event listeners
 menu.addEventListener('click', function(){
     hiddenMenu.classList.toggle('hidden');
     menu.classList.toggle('here');
 })
-main.addEventListener('click', function(){
+articles.addEventListener('click', function(){
     hiddenMenu.classList.add('hidden');
     menu.classList.remove('here');
+    hamburgerMenu.classList.add('push');
+})
+bar.addEventListener('click', function(){
+    hamburgerMenu.classList.remove('push');
+})
+cancel.addEventListener('click', function(){
+    hamburgerMenu.classList.add('push');
 })
