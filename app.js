73f502aaaf445cf2bc6be7ef567691991a6751e3
Amirhossein-parsi <aprs946@gmail.com>
2024-08-28 09:40:54 +0330

    all pages html and css complete

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-
-// const prevScroll = window.scrollBy;
-
-// window.onscroll = function(){
-//     var currentScroll = window.scrollBy;
-//     if(prevScroll >currentScroll){
-//         document.querySelector('.header__menu--low').computedStyleMap.top = "0";
-//     } else {
-//         document.querySelector('.header__menu--low').computedStyleMap.top = "-5rem";
-//     }
-//     prevScroll = currentScroll;
-// }
+// variables
 let menu = document.querySelector('.header__menu--low__title');
 let hiddenMenu = document.querySelector('.header__menu--low__items');
 let main = document.querySelector('.magazine');
+let articles = document.querySelector('.article');
+
+// event listeners
 menu.addEventListener('click', function(){
     hiddenMenu.classList.toggle('hidden');
     menu.classList.toggle('here');
